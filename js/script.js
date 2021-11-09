f6daade855a849678930c1179a7480d8da7e253b
huynhnam1711 <89021166+huynhnam1711@users.noreply.github.com>
2021-11-09 19:46:18 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Javascript: Search
+let searchForm = document.querySelector('.search-form-container');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+// Javascript: Cart
+let cart = document.querySelector('.shopping-cart-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+// Javascript:Login
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    navbar.classList.remove('active');
+}
+
+// Javascipt:navbar
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
